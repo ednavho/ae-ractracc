bed644f51185be6b8277d2d1fccf09d0d8eaccd0
ednavho <ednaho121@gmail.com>
2023-04-27 18:13:48 -0700

    register changes

---
+++ client/src/components/Register.js
@@ -0,0 +1,22 @@
+
+
+
+export default function Login() {
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [registering, setRegistering] = useState(false);
+
+    
+}
+
+return (
+    <div>
+        <h1>Register</h1>
+        
+        <div>
+            <h3>Name</h3>
+        </div>
+    </div>
+)
