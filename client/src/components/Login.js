704a67cf7821129386bad914365f511b58797eb5
ednavho <ednaho121@gmail.com>
2023-05-02 15:44:20 -0700

    adding style for log/reg

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -40,25 +40,27 @@ export default function Login() {
 
 
     return (
-        <div className='login-cont'>
-            <h1>Login</h1>
+        <div className="bg-container">
+            <div className='login-container'>
+                <h1>Login</h1>
 
-            <div>
-                <h3>Email</h3>
-                <input type="text" id="email" className="form-field" value={email}
-                    onChange={(event) => setEmail(event.target.value)}></input>
+                <div className="input-field">
+                    <h3 className="field-label">Email</h3>
+                    <input type="text" id="email" className="form-field" value={email}
+                        onChange={(event) => setEmail(event.target.value)}></input>
+                </div>
+
+                <div className="input-field">
+                    <h3 className="field-label">Password</h3>
+                    <input type="password" id="password" className="form-field" value={password}
+                        onChange={(event) => setPassword(event.target.value)}></input>
+                </div>
+
+                <button className="btn" type="submit" onClick={handleLogin}>Login</button>
+
+                <h3 className="footer" onClick={() => navigate('/register')}>Don't have an account?</h3>
+
             </div>
-
-            <div>
-                <h3>Password</h3>
-                <input type="text" id="password" className="form-field" value={password}
-                    onChange={(event) => setPassword(event.target.value)}></input>
-            </div>
-
-            <button className="btn" type="submit" onClick={handleLogin}>Login</button>
-
-            <h3 className="footer" onClick={() => navigate('/register')}>Don't have an account?</h3>
-
         </div>
     )
 }
