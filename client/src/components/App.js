9fa4d04a4dfdcbf82ffde03c6af94709d524fc85
alexanderyoza <alex.yoza@gmail.com>
2023-05-27 03:30:50 -0700

    download webcam

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,14 +3,11 @@ import Title from './Title';
 import Login from './Login';
 import Register from './Register';
 import Verify from './Verify';
-<<<<<<< HEAD
+
 import Menu from './Menu';
 import Camera from './Camera';
-
-=======
 import UserProfile from './UserProfile';
 import Post from './Post';
->>>>>>> main
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Feed from "./Feed";
@@ -20,18 +17,7 @@ import Feed from "./Feed";
 import React from 'react';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <BrowserRouter>
-      <>
-        <Routes>
-        <Route exact path='/' element={< Camera />}></Route>
-        <Route exact path='/register' element={< Register />}></Route>
-        <Route exact path='/verify' element={< Verify />}></Route>
-        </Routes>
-      </>
-    </BrowserRouter>
-=======
+
    return (
       <div className='App'>
          <BrowserRouter>
@@ -42,13 +28,13 @@ function App() {
                   <Route exact path='/register' element={<Register />} />
                   <Route exact path='/post' element={<Post />} />
                   <Route exact path='/userprofile' element={<UserProfile />} />
+                  <Route exact path='/camera' element={< Camera />}></Route>
                   <Route exact path='/verify' element={<Verify />} />
                </Routes>
             </>
          </BrowserRouter>
       </div>
       
->>>>>>> main
   );
 }
 
